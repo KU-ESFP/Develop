a1585cc5d6738b38a8522aa8ce225c9db1682766
suzn2 <ryusuzn@gmail.com>
2021-10-05 10:43:38 +0900

    뒤로 가기 버튼 앱 종료 에러 해결

--- FeelRing/app/src/main/java/com/FeelRing/activity/SurveyActivity.java
+++ FeelRing/app/src/main/java/com/FeelRing/activity/SurveyActivity.java
@@ -1,6 +1,7 @@
 package com.FeelRing.activity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +14,8 @@ import com.FeelRing.utils.Const;
 public class SurveyActivity extends BaseActivity {
     EditText etInputName;
     Button btNext;
+
+    private long backKeyPressedTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +61,40 @@ public class SurveyActivity extends BaseActivity {
         } else {
             Log.d(Const.TAG, "Nick name is not exist");
             return false;
+        }
+    }
+
+    private void exitProgram() {
+        moveTaskToBack(true); // 태스크를 백그라운드로 이동
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            // 액티비티 종료 + 태스크 리스트에서 지우기
+            finishAndRemoveTask();
+        } else {
+            // 액티비티 종료
+            finish();
+        }
+
+        System.exit(0);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // super.onBackPressed();
+
+        if (System.currentTimeMillis() > backKeyPressedTime + 2500) {
+            backKeyPressedTime = System.currentTimeMillis();
+            showToast(R.string.touch_one_backpress);
+            Log.d(Const.TAG, "aaa back press time == " + backKeyPressedTime);
+            Log.d(Const.TAG, "system time" + System.currentTimeMillis());
+            return;
+        }
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간에 2.5초를 더해 현재 시간과 비교 후
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간이 2.5초가 지나지 않았으면 종료
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2500) {
+            Log.d(Const.TAG, "finish!!");
+
+            exitProgram();
         }
     }
 

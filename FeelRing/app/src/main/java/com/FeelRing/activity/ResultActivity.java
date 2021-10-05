a30cdd71ee73254b7ca56d2deeeee4633c2fe142
suzn2 <ryusuzn@gmail.com>
2021-10-05 10:47:05 +0900

    최종 결과 액티비티 추가

---
+++ FeelRing/app/src/main/java/com/FeelRing/activity/ResultActivity.java
@@ -0,0 +1,16 @@
+package com.FeelRing.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.FeelRing.R;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}

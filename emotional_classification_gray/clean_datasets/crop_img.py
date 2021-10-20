05331e03fed933101cb36165e5e9680d29e10370
yuz <yujn413@naver.com>
2021-10-20 20:45:41 +0900

    사진 넣으면 알아서 얼굴 검출한 뒤에 얼굴 크기만큼 잘라줌

---
+++ emotional_classification_gray/clean_datasets/crop_img.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import cv2
+import torch
+import torchvision.transforms as transforms
+
+from PIL import Image
+
+# Face detection: Load classifiers stored in XML format
+face_cascade = cv2.CascadeClassifier('../trained_models/detection_models/haarcascade_frontalface_default.xml')
+
+direction = './angry_face'
+for image_name in os.listdir(direction):
+    img = cv2.imread(os.path.join(direction, image_name))
+
+    # # 3:4 비율에 맞게 이미지 크기 변환
+    # ratio = 300.0 / img.shape[1]
+    # dim = (300, int(img.shape[0] * ratio))
+    # img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+
+    # Face detection within the image
+    face = face_cascade.detectMultiScale(img, 1.3, 5)
+
+    list_face_detected = []
+    for (x, y, w, h) in face:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        face_boundary = img[y:y + h, x:x + w]
+
+        img_48x48 = cv2.resize(face_boundary, (48, 48), interpolation=cv2.INTER_LINEAR)
+        img_gray = cv2.cvtColor(img_48x48, cv2.COLOR_BGR2GRAY)
+
+        cv2.imwrite('./new_angry/%s' % 'o'+ image_name, img_gray)
+        print("yes")
